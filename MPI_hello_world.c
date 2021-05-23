af52618b6bbf401394d25f9b85ca1e898de05c5d
kevinarnmark <kevin.arnmark@telia.com>
2021-05-23 11:17:37 +0200

    finished exercise 1

--- MPI_hello_world.c
+++ MPI_hello_world.c
@@ -9,7 +9,7 @@ int main(int argc, char *argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    printf("Hello World from rank %f from %f processes!", rank, size);
+    printf("Hello World from rank %d from %d processes!\n", rank, size);
     
     MPI_Finalize();
 }
